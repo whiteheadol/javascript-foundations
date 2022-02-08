ef183dcc2b8ab018f2fc6b884b8df3cf839bfcd9
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-02-07 17:13:02 -0700

    Add completed unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,46 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
+    if (color === undefined) {
+      this.color = 'white';
+    } else {
     this.color = color;
+    }
   }
-}
+  isWhite() {
+    if (this.color !== 'white') {
+      return false;
+      }
+    }
+
+  says(message) {
+    return`**;* ${message} *;**`;
+  }
+  };
+
+// class Unicorn {
+//   constructor(name, color) {
+//     this.name = name;
+//     if (color === undefined) {
+//       this.color = 'white';
+//     } else {
+//     this.color = color;
+//     }
+//   }
+//   isWhite() {
+//     // if (this.color === 'white') {
+//     //   return true;
+//     // } else {
+//     //   return false;
+//     }
+//   }
+// }
 
 module.exports = Unicorn;
+
+//1. Read the test
+//2. Ask yourself if it's going to fail and why
+//3. Run the test
+//4. Make it pass
+
+//assert.equal(actual, expected);
