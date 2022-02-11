4049591e6052fc38814ba64d5d692280429e9bcb
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-02-10 22:37:37 -0700

    Add completed pirate and updated README

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,35 @@
+class Magician {
+  constructor(magician) {
+    this.name = `The Great ${magician.name}`;
+    this.assistant = magician.assistant;
+    this.favoriteAccessory = magician.clothing || "top hat";
+    this.confidencePercentage = 10;
+  }
+  performIncantation(spell) {
+    return spell.toUpperCase() + "!";
+  }
+  performTrick() {
+    this.confidencePercentage += 10;
+    if (this.favoriteAccessory === "top hat") {
+      return "PULL RABBIT FROM TOP HAT";
+    } else {
+      return "PULL DOVE FROM SLEEVE";
+    }
+  }
+  // performShowStopper() {
+  //   if (this.confidencePercentage < 100) {
+  //     return "Oh no, this trick is not ready!";
+  //   } else {
+  //     return "WOW! The magician totally just sawed that person in half!";
+  //   }
+  // }
+  performShowStopper() {
+    if (this.confidencePercentage >= 100 && this.assistant) {
+      return "WOW! The magician totally just sawed that person in half!";
+    } else {
+      return "Oh no, this trick is not ready!";
+    }
+  }
+};
+
+module.exports = Magician;
