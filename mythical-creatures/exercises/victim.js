4384115395ee128397418820231ef7c1319e318f
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-02-12 20:44:16 -0700

    Add completed werewolf and updated README

---
+++ mythical-creatures/exercises/victim.js
@@ -0,0 +1,8 @@
+class Victim {
+  constructor(victimInput) {
+    this.name = victimInput;
+    this.alive = true;
+  }
+};
+
+module.exports = Victim;
---
+++ mythical-creatures/exercises/werewolf.js
@@ -0,0 +1,32 @@
+var Victim = require('./victim');
+
+class Werewolf {
+  constructor(nameInput) {
+    this.name = nameInput;
+    this.form = 'human';
+    this.hungry = false;
+  }
+
+  completeTransformation() {
+    if (this.form === 'human') {
+    this.form = 'wolf';
+    this.hungry = true;
+    return `Aaa-Woooo!`;
+    } else {
+    this.form = 'human';
+    this.hungry = false;
+    return `Where are I?`;
+    }
+  };
+
+  eatVictim(victim) {
+    if(this.hungry && this.form === 'wolf') {
+      victim.alive = false;
+      this.form = 'human';
+      return `Yum, ${victim.name} was delicious.`
+    }
+    return `No way am I eating ${victim.name}, I'd like a burger!`;
+  };
+}
+
+module.exports = Werewolf;
