537f4914b60bcaa3d6915851648ef5282f1258cf
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-02-25 09:00:12 -0700

    Add some of vending machine

--- vending-machine/src/snack.js
+++ vending-machine/src/snack.js
@@ -1,1 +1,23 @@
+class Snack {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.itemsInStock = 0;
+  }
+  
+//A better variable name here, not plural like an array
+  stockItems(items) {
+    this.itemsInStock += items;
+  }
 
+  removeItem() {
+    if (this.itemsInStock) {
+      this.itemsInStock--;
+      return `Item taken! There are now ${this.itemsInStock} items left.`;
+    } else {
+      return `Sorry, no ${this.name} left in stock!`;
+    }
+  }
+};
+
+module.exports = Snack;
---
+++ vending-machine/src/vending-machine.js
@@ -0,0 +1,23 @@
+class VendingMachine {
+  constructor(machine) {
+    this.id = machine.id;
+    this.isBroken = machine.isBroken;
+    this.snacks = [];
+  }
+
+  addSnacks(snack) {
+    for (var i = 0; i <= this.snacks.length; i++) {
+      if (this.snacks.length === 0 || snack !== this.snacks[i]) {
+        this.snacks.push(snack);
+      } else {
+        return this.snacks;
+      }
+    }
+  }
+}
+
+//Check the < vs <= more often, or more quickly
+//Not good practice to compare two objects, use .name instead of thte entire objects
+//Primitives always preferred when comparing
+
+module.exports = VendingMachine;
